e7abf39ebbd95750e1def7d142e1a2177c24eed7
umarfaisal295c <umarfaisalumarfaisal447@gmail.com>
2023-11-02 21:18:22 +0500

    Add another file 02_dataTypes

---
+++ Chai_JS/02_dataTypes.js
@@ -0,0 +1,21 @@
+/*
+    we cannot ues alert in JS because we use Js In nodeJS and alert use in Browser.
+    alert(3+3);
+*/
+// but console will be use in nodeJS.
+console.log(3+3);
+
+/*
+    DataTypes in JS.
+    number 
+    string => ""
+    boolean => true/false
+    bigint => for bigger value
+    null => empty value
+    undefined
+    symbol => unique
+*/
+
+// in this JS we use "typeof" operator .
+console.log(typeof "Umar")
+// used for understand the what type of data type in JS
