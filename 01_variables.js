c950e893ae8d6d23e1f5eb750813d79628dface5
umarfaisal295c <umarfaisalumarfaisal447@gmail.com>
2023-11-02 20:55:50 +0500

    Add 01_Variables in JS

---
+++ 01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 13445;
+let accountEmail = "umar@.com";
+var accountPassword = "33445";
+accountCity = "Lahore";
+let accountState;
+// accountId=12345
+console.log(accountId);
+console.table([
+  accountId,
+  accountEmail,
+  accountPassword,
+  accountCity,
+  accountState,
+]);
