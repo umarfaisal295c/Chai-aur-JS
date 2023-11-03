5fc7a30f0db336b65273611154d8be209012c4e9
umarfaisal295c <umarfaisalumarfaisal447@gmail.com>
2023-11-03 21:52:25 +0500

    Added about Operations in JS in 03_conversionOperations

--- Chai_JS/03_conversionOperators.js
+++ Chai_JS/03_conversionOperators.js
@@ -1,22 +1,22 @@
-// if we convert a number to other DataType this is called 
+// if we convert a number to other DataType this is called
 // conversion operator.
 
-let scroe="33"
-let isNumber = Number(scroe)
+let scroe = "33";
+let isNumber = Number(scroe);
 // console.log(typeof isNumber);
 // console.log( isNumber);
 
-// 
-let newScore="33abc"
-let isNewNumber=Number(newScore)
+//
+let newScore = "33abc";
+let isNewNumber = Number(newScore);
 
 // console.log(typeof isNewNumber);
 // console.log(isNewNumber);
 
 // 33 => number
 // 33 => NaN (Not a Number)
-let isLoggedIn=1;
-let isBoolean=Boolean(isLoggedIn)
+let isLoggedIn = 1;
+let isBoolean = Boolean(isLoggedIn);
 // console.log(typeof isBoolean);
 // console.log(isBoolean);
 
@@ -24,7 +24,35 @@ let isBoolean=Boolean(isLoggedIn)
 //  "" => false
 //  "umar" => true
 
-let    someNumber=33
-let stringNumber=   String(someNumber)
-console.log(typeof stringNumber);
-console.log(stringNumber);
+let someNumber = 33;
+let stringNumber = String(someNumber);
+// console.log(typeof stringNumber);
+// console.log(stringNumber);
+
+// ******************* Operators ****************** \\
+
+// 2 + 2 =4 // but + is operaors in js.
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%2);
+
+let str1 = "Heloo";
+let str2 = " Muhammad Umar";
+let str3 = str1 + str2;
+// console.log(str3);
+//
+// console.log("1"+2);
+// console.log(1+"2");
+// console.log("1"+2+2);
+// console.log(1+2+"2");
+// console.log((3+4)*5%3);
+
+//
+let gameCounter = 100;
+// gameCounter++ Postfix
+++gameCounter; // Prefix
+console.log(gameCounter);
