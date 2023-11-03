d2d5fa5e64f9d78319227150fc32a41f0541bbc1
umarfaisal295c <umarfaisalumarfaisal447@gmail.com>
2023-11-03 20:08:44 +0500

    Added new file Conversion Operator

---
+++ Chai_JS/03_conversionOperators.js
@@ -0,0 +1,30 @@
+// if we convert a number to other DataType this is called 
+// conversion operator.
+
+let scroe="33"
+let isNumber = Number(scroe)
+// console.log(typeof isNumber);
+// console.log( isNumber);
+
+// 
+let newScore="33abc"
+let isNewNumber=Number(newScore)
+
+// console.log(typeof isNewNumber);
+// console.log(isNewNumber);
+
+// 33 => number
+// 33 => NaN (Not a Number)
+let isLoggedIn=1;
+let isBoolean=Boolean(isLoggedIn)
+// console.log(typeof isBoolean);
+// console.log(isBoolean);
+
+//  1 => true , 0 => false;
+//  "" => false
+//  "umar" => true
+
+let    someNumber=33
+let stringNumber=   String(someNumber)
+console.log(typeof stringNumber);
+console.log(stringNumber);
