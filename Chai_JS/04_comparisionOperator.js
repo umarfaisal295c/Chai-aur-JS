617c702ec4f188ffcbe870b97fcc6c4c2bfddc66
umarfaisal295c <umarfaisalumarfaisal447@gmail.com>
2023-11-06 19:40:30 +0500

    New file add in Chai_JS

---
+++ Chai_JS/04_comparisionOperator.js
@@ -0,0 +1,26 @@
+// >  => less then 
+// <  => greater then
+// >=  => less then 0r equal then
+// <=  =>  greater then 0r equal then
+// ==  =>  equal then
+// ===  => datatype are equal
+//  This is called Comparision Operator in JS.
+
+// Example:-
+//
+    console.log(2>1);
+    console.log(2>=1);
+    console.log(2<=1);
+//
+// 
+    console.log("2">1);
+    console.log("02"==1);
+//
+    console.log(null>0);
+    console.log(null==0);
+    console.log(null>=0);
+//
+//
+    console.log("2"===2);
+    console.log(2===2);
+//
